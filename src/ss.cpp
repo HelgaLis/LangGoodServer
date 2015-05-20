abaada73214f8dc12dd81362242ed067a820eab9
helgalis <helgalis@andy-1015PW>
2015-05-20 05:45:46 +0400

    t

---
+++ src/ss.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : ss.cpp
+// Author      : hl
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
