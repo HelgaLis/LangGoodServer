d95da34614cc1057f29461ab249399bce08b5b21
helgalis <helgalis@andy-1015PW>
2015-05-23 19:09:23 +0400

    lost header

--- src/server_interface/message.h
+++ src/server_interface/message.h
@@ -5,6 +5,11 @@
  *      Author: helgalis
  */
 
+#ifndef SRC_SERVER_INTERFACE_MESSAGE_H_
+#define SRC_SERVER_INTERFACE_MESSAGE_H_
+
 
 void error(int status, int err, const char* msg);
 
+
+#endif /* SRC_SERVER_INTERFACE_MESSAGE_H_ */
