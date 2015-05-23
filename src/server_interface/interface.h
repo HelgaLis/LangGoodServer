3a73b76098fd34b0d148bcc0f0cb68fc34f39999
helgalis <helgalis@andy-1015PW>
2015-05-23 23:38:23 +0400

    add functionality

--- src/server_interface/interface.h
+++ src/server_interface/interface.h
@@ -7,11 +7,17 @@
 
 #ifndef SRC_SERVER_INTERFACE_INTERFACE_H_
 #define SRC_SERVER_INTERFACE_INTERFACE_H_
-
-
+using namespace std;
+#include <iostream>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
 void start_engine();
+char read_byte(bool & ok);
 char * read_line();
-void send();
+string getFirstWord(string &fLine);
+void sendMsg();
 
 
 #endif /* SRC_SERVER_INTERFACE_INTERFACE_H_ */
